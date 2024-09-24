208d8f8bc380b8c656e290a3192e8d1ea86b149c
vijayprasanth1234 <vijayprasanth2999@gmail.com>
2024-09-24 14:28:39 +0530

    vv

---
+++ destructure.js
@@ -0,0 +1,65 @@
+// let arr=[1,2,3,4,5];
+// [a,b,c,d]=arr;
+// console.log(c,d)
+
+// let arr=[1,2,3,4,5];
+// [a,,,b]=arr;
+// console.log(a,b)
+
+
+// let obj={"name":"vijay","city":"hyderabad"}
+// let {city}=obj;
+// console.log(city)
+
+//reassigning the value
+// const obj = { a: 1, b: { c: 2 } };
+// const { a } = obj; 
+// let {b: { c: d },
+// } = obj; 
+// console.log(d)
+
+//rest operator
+
+// let fruits=["apple","grapes","orange","green"];
+// [...remaining]=fruits;
+// let veggies=["carrot","tomato","finger","ove"];
+// [...some]=veggies;
+// console.log([...fruits,...veggies])
+// console.log(...fruits)
+// console.log(fruits,veggies)
+// console.log(fruits+veggies)
+// console.log([fruits,veggies])
+
+
+//for objects
+
+// let obj1={"domain":"vijaudev","me":"sus"};
+// console.log({...obj,...obj1})
+
+//functions
+// function demo(...x){
+//     console.log(x)
+
+// }
+// demo(1,2,3,4,5)
+
+
+// rest operator
+
+// function demo(...x){
+//     console.log(x)
+
+// }
+// demo(1,2,3)
+
+
+
+// function demo1(y){
+//     console.log(y)
+// }
+// let arr=["a","b","c"]
+// demo1(...arr)
+
+
+let res=10*30
+console.log(res)
